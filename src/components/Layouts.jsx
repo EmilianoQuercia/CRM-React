0095c5c76c0163ecfc3187a3f24586a83a0177c0
EmilianoQuercia <emilianoquercia1@gmail.com>
2023-02-15 12:52:45 -0300

    creacion de proyecto, utilizando tailwindcss y react router dom

---
+++ src/components/Layouts.jsx
@@ -0,0 +1,21 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layouts = () => {
+    return (
+      <div className="md:flex md:min-h-screen">
+        <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+            <h2 className="text-4xl font-black text-center text-white">CRM - Clientes</h2>
+            <nav className="mt-10">
+                <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/">Clientes</Link>
+                <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/clientes/nuevo">Nuevo Cliente</Link>
+            </nav>
+        </aside>
+        <div className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+            <Outlet />
+        </div>
+
+      </div>
+    );
+}
+
+export default Layouts;
